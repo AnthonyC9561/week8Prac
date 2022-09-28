8f6ba90dc6648782606b079d2cb0b8e65f63aae6
LachlanMuir <53891974+LachlanMuir@users.noreply.github.com>
2022-09-28 17:10:02 +1000

    Player Movement Added

--- My project (1)/Assets/Scripts/PlayerMovement.cs
+++ My project (1)/Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-
     Rigidbody rb;
-void Start(){
 
- float speed = 5.0f;
-    rb = GetComponent<Rigidbody>();
-}
- 
-    
+    void Start()
+    {
+        float speed = 5.0f;
+        rb = GetComponent<Rigidbody>();
+    }
 
-
- void Update(){
+ void Update()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         rb.velocity = new Vector3(horizontal * speed, rb.velocity.y, vertical * speed);
-
- 
+        transform.Translate(rb.velocity * Time.deltaTime);
     }
 }
