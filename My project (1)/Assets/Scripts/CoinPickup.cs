b94ea68fae8a6e64521ac68ed5bd9b3d5bf31def
LachlanMuir <53891974+LachlanMuir@users.noreply.github.com>
2022-09-28 17:35:22 +1000

    Score Added

--- My project (1)/Assets/Scripts/CoinPickup.cs
+++ My project (1)/Assets/Scripts/CoinPickup.cs
@@ -4,29 +4,11 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
- public void OnTriggerEnter(Collider collider)
-    {
-                Destroy(gameObject);
-=======
     
  public void OnTriggerEnter(Collider collider)
     {
         ScoreKeeper.Instance.addCoinScore();
         Destroy(gameObject);
->>>>>>> Stashed changes
     }
 
 }
