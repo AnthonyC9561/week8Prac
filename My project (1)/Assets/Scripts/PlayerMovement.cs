89aeca7daa8c7b19f89b7a09aa34986fe64f9005
MR Anthony Cheong <anthony.cheong@students.mq.edu>
2022-10-04 14:20:06 +1100

    changed PlayerMovement transform.Translate

--- My project (1)/Assets/Scripts/PlayerMovement.cs
+++ My project (1)/Assets/Scripts/PlayerMovement.cs
@@ -5,27 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    Rigidbody rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
 
     void Update()
     {
-        float horizontal = Input.GetAxis(InputAxes.Horizontal);
-        float vertical = Input.GetAxis(InputAxes.Vertical);
 
-        rb.velocity = new Vector3(horizontal * speed, rb.velocity.y, vertical * speed);
-        transform.Translate(rb.velocity * Time.deltaTime);
+        Vector3 velocity = new Vector3();
 
-        //maybe this way below so we don't need to get RigidBody component?
-        // Vector3 velocity = new Vector3();
+        velocity.x = Input.GetAxis(InputAxes.Horizontal);
+        velocity.z = Input.GetAxis(InputAxes.Vertical);
 
-        // velocity.x = Input.GetAxis(InputAxes.Horizontal);
-        // velocity.z = Input.GetAxis(InputAxes.Vertical);
-
-        // transform.Translate(velocity * speed * Time.deltaTime);
+        transform.Translate(velocity * speed * Time.deltaTime);
     }
 }
